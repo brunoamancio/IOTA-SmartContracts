3b35dbba38deb0fe875ba7dfbb6913f9433035a7
Th3B0Y <brunoamancio.ti@gmail.com>
2021-02-15 01:58:41 +0100

    Implemented logic to ensure function caller is contract creator or chain owner. Added functions using that functionality.

--- smartcontract/rust/src/lib.rs
+++ smartcontract/rust/src/lib.rs
@@ -1,16 +1,37 @@
 use wasmlib::*;
+mod sc_utils;
 
 #[no_mangle]
 fn on_load() {
     let exports = ScExports::new();
-    exports.add_func("my_sc_request", my_sc_request);
+
+    // Functions
+    exports.add_func("my_sc_function", my_sc_function);
+    exports.add_func("contract_creator_only_function", contract_creator_only_function);
+    exports.add_func("chain_owner_only_function", chain_owner_only_function);
+
+    // Views
     exports.add_view("my_sc_view", my_sc_view);
 }
 
-fn my_sc_request(ctx: &ScFuncContext) {
-    ctx.log("my_sc_request");
+// Anyone can call this SC-Function
+fn my_sc_function(ctx: &ScFuncContext) {
+    ctx.log("my_sc_function");
 }
 
+// Only the contract creator can call this SC-Function
+fn contract_creator_only_function(ctx: &ScFuncContext) {
+    sc_utils::caller_must_be_contract_creator(ctx);
+    ctx.log("Caller is the contract creator =)");
+}
+
+// Only the chain owner can call this SC-Function
+fn chain_owner_only_function(ctx: &ScFuncContext){
+    sc_utils::caller_must_be_chain_owner(ctx);
+    ctx.log("Caller is the chain owner =)");
+}
+
+// Public view
 fn my_sc_view(ctx: &ScViewContext) {
     ctx.log("my_sc_view");
 }
