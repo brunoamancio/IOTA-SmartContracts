426ac3f0404cc4327582233aa814c55757367de8
Th3B0Y <brunoamancio.ti@gmail.com>
2021-02-22 20:19:51 +0100

    Added safe math

--- smartcontract/rust/iota_sc_utils/src/lib.rs
+++ smartcontract/rust/iota_sc_utils/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod access;
 pub mod params;
+pub mod math;
--- smartcontract/rust/src/lib.rs
+++ smartcontract/rust/src/lib.rs
@@ -1,30 +1,40 @@
 use wasmlib::*;
 use iota_sc_utils::*;
+pub mod math_test_functions;
 
 #[no_mangle]
 fn on_load() {
     let exports = ScExports::new();
 
-    // Functions
+    // SC functions
     exports.add_func("my_sc_function", my_sc_function);
     exports.add_func("contract_creator_only_function", contract_creator_only_function);
     exports.add_func("chain_owner_only_function", chain_owner_only_function);
 
-    // Views
+    // SC functions - Used to test the safe-mathmodule.
+    math_test_functions::register_math_sc_functions(&exports);
+
+    // SC Views
     exports.add_view("my_sc_view", my_sc_view);
 }
 
-// Anyone can call this SC-Function
+// Anyone can call this SC function
 fn my_sc_function(ctx: &ScFuncContext) {
     // Logs a text
     ctx.log("my_sc_function");
-
+    
     // Reads argument called "my_param" passed to SC function. Empty if not found.
     const MY_PARAM : &str = "my_param";
     let param_value = params::get_string(MY_PARAM, ctx);
     if !param_value.is_empty() {
         ctx.log(&param_value);
     }
+
+    // Uses safe logic to perform an addition. The SC function panics on over/under flows.
+    let my_i64 : i64 = 0;
+    let my_i64_to_add : i64 = 1;
+    // Alternative syntax: my_i64.safe_add(&my_i64_to_add, ctx);
+    let _result = math::SafeMath::safe_add(&my_i64, &my_i64_to_add, ctx); 
 }
 
 // Only the contract creator can call this SC-Function
