6061eeb771a341cebe703d1190b05caca14a467d
Th3B0Y <brunoamancio.ti@gmail.com>
2021-03-03 01:13:13 +0100

    Updated wasp to use develop branch, commit df34b04

--- smartcontract/rust/iota_sc_utils/src/params.rs
+++ smartcontract/rust/iota_sc_utils/src/params.rs
@@ -22,14 +22,14 @@ pub fn get_string<TContext : ScBaseContext>(param_name : &str, ctx: &TContext) -
 
 /// Tries to get i64 parameter. Panics if it can't find it.
 pub fn must_get_i64<TContext : ScBaseContext>(param_name : &str, ctx: &TContext) -> i64 {
-    let param = ctx.params().get_int(param_name);
+    let param = ctx.params().get_int64(param_name);
     ctx.require(param.exists(), "string parameter not found");
     return param.value();
-}
+} 
 
 /// Tries to get i64 parameter. Returns empty &str if it can't find it.
 pub fn get_i64<TContext : ScBaseContext>(param_name : &str, ctx: &TContext) -> i64 {
-    let param = ctx.params().get_int(param_name);
+    let param = ctx.params().get_int64(param_name);
     if param.exists() {
         return param.value();
     }
