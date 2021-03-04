01a927d5711cb5acf4728cbd8e80ee36ec5db61e
Th3B0Y <brunoamancio.ti@gmail.com>
2021-03-04 04:20:47 +0100

    FInished rewriting tests to use "NotSolo"

--- tests/testutils/codesamples/chain_samples_test.go
+++ tests/testutils/codesamples/chain_samples_test.go
@@ -4,150 +4,124 @@ import (
 	"testing"
 
 	"github.com/brunoamancio/IOTA-SmartContracts/tests/testutils/testconstants"
+	notsolo "github.com/brunoamancio/NotSolo"
 	"github.com/iotaledger/goshimmer/dapps/valuetransfers/packages/balance"
-	"github.com/iotaledger/wasp/packages/coretypes"
-	"github.com/iotaledger/wasp/packages/solo"
-	"github.com/iotaledger/wasp/packages/vm/core/accounts"
-	"github.com/iotaledger/wasp/packages/vm/core/root"
 	"github.com/stretchr/testify/require"
 )
 
 func Test_CreateChain_chainCreatorSpecified(t *testing.T) {
-	env := solo.New(t, testconstants.Debug, testconstants.StackTrace)
+	notSolo := notsolo.New(t)
 
-	// Create address with dummy tokens in it.
-	chainOriginatorsWalletKeyPair := env.NewSignatureSchemeWithFunds()
-	require.NotNil(t, chainOriginatorsWalletKeyPair)
+	// Create a signature scheme with dummy tokens in it.
+	chainOriginatorsSigScheme := notSolo.SigScheme.NewSignatureSchemeWithFunds()
 
-	// Wallet addresses
-	chainOriginatorsWalletAddress := chainOriginatorsWalletKeyPair.Address()
-	require.NotNil(t, chainOriginatorsWalletAddress)
+	// Balance in value tangle before chain is created
+	notSolo.ValueTangle.RequireBalance(chainOriginatorsSigScheme, balance.ColorIOTA, initialWalletFunds)
 
-	// Wallet balance in value tangle before chain is created
-	env.AssertAddressBalance(chainOriginatorsWalletAddress, balance.ColorIOTA, initialWalletFunds)
-
-	// Create a chain where chainCreatorsWalletKeyPair is the owner.
-	chain := env.NewChain(chainOriginatorsWalletKeyPair, "myChain")
-	require.NotNil(t, chain)
+	// Create a chain where chainOriginatorsSigScheme is the owner.
+	chain := notSolo.Chain.NewChain(chainOriginatorsSigScheme, "myChain")
 	require.NotEqual(t, chain.ChainColor, balance.ColorIOTA)
 
-	// Wallet balance in value tangle after chain is created
-	env.AssertAddressBalance(chainOriginatorsWalletAddress, balance.ColorIOTA, initialWalletFunds-iotaTokensConsumedByRequest-iotaTokensConsumedByChain)
+	// Balance in value tangle after chain is created
+	notSolo.ValueTangle.RequireBalance(chainOriginatorsSigScheme, balance.ColorIOTA, initialWalletFunds-iotaTokensConsumedByRequest-iotaTokensConsumedByChain)
 
 	// IMPORTANT: When a chain is created, 1 IOTA is colored with the chain's color and sent to the chain's address in the value tangle
-	env.AssertAddressBalance(chain.ChainAddress, chain.ChainColor, iotaTokensConsumedByChain)
-
-	// AgentID of the wallet (also, chain.OriginatorAgentID)
-	chainOriginatorsAgentID := coretypes.NewAgentIDFromAddress(chainOriginatorsWalletAddress)
+	notSolo.ValueTangle.RequireAddressBalance(chain.ChainAddress, chain.ChainColor, iotaTokensConsumedByChain)
 
 	// IMPORTANT: When a chain is created, 1 IOTA is sent from the originator's account in the value tangle their account in the chain
-	chain.AssertAccountBalance(chainOriginatorsAgentID, balance.ColorIOTA, iotaTokensConsumedByRequest)
+	notSolo.Chain.RequireBalance(chainOriginatorsSigScheme, chain, balance.ColorIOTA, iotaTokensConsumedByRequest)
 }
 
 // Sample of how to create chain without specifying a chainOriginator.
 // A dummy chain originator is created in the background (by NewChain).
 func Test_CreateChain_NoChainCreatorSpecified(t *testing.T) {
-	env := solo.New(t, testconstants.Debug, testconstants.StackTrace)
+	notSolo := notsolo.New(t)
 
-	// Create a chain where chainCreatorsWalletKeyPair is the owner.
-	chain := env.NewChain(nil, "myChain")
-	require.NotNil(t, chain)
+	// Create a chain where chainOriginatorsSigScheme is the owner.
+	chain := notSolo.Chain.NewChain(nil, "myChain")
 	require.NotEqual(t, chain.ChainColor, balance.ColorIOTA)
 
 	// IMPORTANT: When a chain is created, 1 IOTA is colored with the chain's color and sent to the chain's address in the value tangle
-	env.AssertAddressBalance(chain.ChainAddress, chain.ChainColor, iotaTokensConsumedByChain)
+	notSolo.ValueTangle.RequireBalance(chain.OriginatorSigScheme, chain.ChainColor, iotaTokensConsumedByChain)
 
 	// IMPORTANT: When a chain is created, 1 IOTA is sent from the originator's account in the value tangle their account in the chain
-	chain.AssertAccountBalance(chain.OriginatorAgentID, balance.ColorIOTA, iotaTokensConsumedByRequest)
+	notSolo.Chain.RequireBalance(chain.OriginatorSigScheme, chain, balance.ColorIOTA, iotaTokensConsumedByRequest)
 }
 
 func Test_SetChainFees(t *testing.T) {
-	env := solo.New(t, testconstants.Debug, testconstants.StackTrace)
+	notSolo := notsolo.New(t)
 
 	// Generate a dummy chain beloging to chain.OriginatorSigScheme
-	chain := env.NewChain(nil, "myChain")
+	chain := notSolo.Chain.NewChain(nil, "myChain")
 
 	// Initial chain fees
-	feeColor, ownerFee, validatorFee := chain.GetFeeInfo(accounts.Name)
+	feeColor, ownerFee, validatorFee := chain.GetFeeInfo(testconstants.AccountsContractName)
 	require.Equal(t, balance.ColorIOTA, feeColor)
 	require.Equal(t, int64(0), ownerFee)
 	require.Equal(t, int64(0), validatorFee)
 
-	// Request to chain - change owner fee settings
+	// Request to chain - change contract owner fee settings
 	const newChainOwnerFee = int64(1000)
-	transferRequest := solo.NewCallParams(root.Interface.Name, root.FuncSetContractFee, root.ParamHname, accounts.Interface.Hname(), root.ParamOwnerFee, newChainOwnerFee)
-	_, err := chain.PostRequestSync(transferRequest, chain.OriginatorSigScheme)
-	require.NoError(t, err)
+	notSolo.Chain.ChangeContractFees(chain.OriginatorSigScheme, chain, testconstants.AccountsContractName, newChainOwnerFee)
 
-	// Request to chain - change owner fee settings
+	// Request to chain - change validator fee settings
 	const newValidatorFee = int64(200)
-	transferRequest = solo.NewCallParams(root.Interface.Name, root.FuncSetContractFee, root.ParamHname, accounts.Interface.Hname(), root.ParamValidatorFee, newValidatorFee)
-	_, err = chain.PostRequestSync(transferRequest, chain.OriginatorSigScheme)
-	require.NoError(t, err)
+	notSolo.Chain.ChangeValidatorFees(chain.OriginatorSigScheme, chain, testconstants.AccountsContractName, newValidatorFee)
 
 	// Chain fees after change
-	feeColor, ownerFee, validatorFee = chain.GetFeeInfo(accounts.Name)
+	feeColor, ownerFee, validatorFee = chain.GetFeeInfo(testconstants.AccountsContractName)
 	require.Equal(t, balance.ColorIOTA, feeColor)
 	require.Equal(t, newChainOwnerFee, ownerFee)
 	require.Equal(t, newValidatorFee, validatorFee)
 }
 
 func Test_SetChainFees_TestCharge(t *testing.T) {
-	env := solo.New(t, testconstants.Debug, testconstants.StackTrace)
+	notSolo := notsolo.New(t)
 
 	// Generate a keypair for the chain originator
-	chainOriginatorKeyPair := env.NewSignatureSchemeWithFunds()
-	chainOriginatorAddress := chainOriginatorKeyPair.Address()
-	chainOriginatorAgentID := coretypes.NewAgentIDFromAddress(chainOriginatorAddress)
-	require.NotNil(t, chainOriginatorKeyPair)
-	env.AssertAddressBalance(chainOriginatorAddress, balance.ColorIOTA, initialWalletFunds)
+	chainOriginatorKeyPair := notSolo.SigScheme.NewSignatureSchemeWithFunds()
+	notSolo.ValueTangle.RequireBalance(chainOriginatorKeyPair, balance.ColorIOTA, initialWalletFunds)
 
 	// Generate a keypair for a wallet using the chain (will be charged for it)
-	userKeyPair := env.NewSignatureSchemeWithFunds()
-	userAddress := userKeyPair.Address()
-	userAgentID := coretypes.NewAgentIDFromAddress(userAddress)
-	require.NotNil(t, userKeyPair)
-	env.AssertAddressBalance(userAddress, balance.ColorIOTA, initialWalletFunds)
+	userKeyPair := notSolo.SigScheme.NewSignatureSchemeWithFunds()
+	notSolo.ValueTangle.RequireBalance(userKeyPair, balance.ColorIOTA, initialWalletFunds)
 
 	// Generate a dummy chain beloging to the chain originator
-	chain := env.NewChain(chainOriginatorKeyPair, "myChain")
+	chain := notSolo.Chain.NewChain(chainOriginatorKeyPair, "myChain")
 	chainOriginatorBalanceInValueTangleAfterChainIsCreated := int64(initialWalletFunds - iotaTokensConsumedByRequest - iotaTokensConsumedByChain)
 	chainOriginatorBalanceInChainAfterChainIsCreated := int64(iotaTokensConsumedByRequest)
-	env.AssertAddressBalance(chainOriginatorAddress, balance.ColorIOTA, chainOriginatorBalanceInValueTangleAfterChainIsCreated)
-	env.AssertAddressBalance(chain.ChainAddress, chain.ChainColor, iotaTokensConsumedByChain)
-	chain.AssertAccountBalance(chainOriginatorAgentID, balance.ColorIOTA, chainOriginatorBalanceInChainAfterChainIsCreated)
+	notSolo.ValueTangle.RequireBalance(chainOriginatorKeyPair, balance.ColorIOTA, chainOriginatorBalanceInValueTangleAfterChainIsCreated)
+	notSolo.ValueTangle.RequireAddressBalance(chain.ChainAddress, chain.ChainColor, iotaTokensConsumedByChain)
+	notSolo.Chain.RequireBalance(chain.OriginatorSigScheme, chain, balance.ColorIOTA, chainOriginatorBalanceInChainAfterChainIsCreated)
 
 	// Initial chain fees
-	feeColor, ownerFee, validatorFee := chain.GetFeeInfo(accounts.Name)
+	feeColor, ownerFee, validatorFee := chain.GetFeeInfo(testconstants.AccountsContractName)
 	require.Equal(t, balance.ColorIOTA, feeColor)
 	require.Equal(t, int64(0), ownerFee)
 	require.Equal(t, int64(0), validatorFee)
 
 	// Request to chain change fee settings
 	const newOwnerFee = int64(100)
-	transferRequest := solo.NewCallParams(root.Interface.Name, root.FuncSetContractFee, root.ParamHname, accounts.Interface.Hname(), root.ParamOwnerFee, newOwnerFee)
-	_, err := chain.PostRequestSync(transferRequest, chainOriginatorKeyPair)
-	require.NoError(t, err)
+
+	notSolo.Chain.ChangeContractFees(chain.OriginatorSigScheme, chain, testconstants.AccountsContractName, newOwnerFee)
 	chainOriginatorBalanceInValueTangleAfterFeeIsChanged := chainOriginatorBalanceInValueTangleAfterChainIsCreated - iotaTokensConsumedByRequest
 	chainOriginatorBalanceInChainAfterFeeIsChanged := chainOriginatorBalanceInChainAfterChainIsCreated + iotaTokensConsumedByRequest
 
 	// Chain fees after change
-	feeColor, ownerFee, validatorFee = chain.GetFeeInfo(accounts.Name)
+	feeColor, ownerFee, validatorFee = chain.GetFeeInfo(testconstants.AccountsContractName)
 	require.Equal(t, balance.ColorIOTA, feeColor)
 	require.Equal(t, newOwnerFee, ownerFee)
 	require.Equal(t, int64(0), validatorFee)
 
 	// User sends a request to the chain (He wants to deposit iotas to his own account in the chain)
-	transferRequest = solo.NewCallParams(accounts.Name, accounts.FuncDeposit).WithTransfer(balance.ColorIOTA, transferValueIotas)
-	_, err = chain.PostRequestSync(transferRequest, userKeyPair)
-	require.NoError(t, err)
+	notSolo.ValueTangle.TransferToChainToSelf(userKeyPair, chain, balance.ColorIOTA, transferValueIotas)
 
 	// User gets the iota tokens in his account in the chain
-	env.AssertAddressBalance(userAddress, balance.ColorIOTA, initialWalletFunds-transferValueIotas-iotaTokensConsumedByRequest) // Transfered tokens are debited from the value tangle
+	notSolo.ValueTangle.RequireBalance(userKeyPair, balance.ColorIOTA, initialWalletFunds-transferValueIotas-iotaTokensConsumedByRequest) // Transfered tokens are debited from the value tangle
 	// Fee is charged from the funds transfered with the request
-	chain.AssertAccountBalance(userAgentID, balance.ColorIOTA, transferValueIotas+iotaTokensConsumedByRequest-ownerFee) // His tokens in the chain minus fees
+	notSolo.Chain.RequireBalance(userKeyPair, chain, balance.ColorIOTA, transferValueIotas+iotaTokensConsumedByRequest-ownerFee) // His tokens in the chain minus fees
 
 	// Chain owner gets fees from the operation
-	env.AssertAddressBalance(chainOriginatorAddress, balance.ColorIOTA, chainOriginatorBalanceInValueTangleAfterFeeIsChanged)      // No change in value tangle
-	chain.AssertAccountBalance(chainOriginatorAgentID, balance.ColorIOTA, chainOriginatorBalanceInChainAfterFeeIsChanged+ownerFee) // He gets the fees in the chain
+	notSolo.ValueTangle.RequireBalance(chainOriginatorKeyPair, balance.ColorIOTA, chainOriginatorBalanceInValueTangleAfterFeeIsChanged)     // No change in value tangle
+	notSolo.Chain.RequireBalance(chainOriginatorKeyPair, chain, balance.ColorIOTA, chainOriginatorBalanceInChainAfterFeeIsChanged+ownerFee) // He gets the fees in the chain
 }
--- tests/testutils/testconstants/test_constants.go
+++ tests/testutils/testconstants/test_constants.go
@@ -7,4 +7,7 @@ const (
 	Debug = false
 	// StackTrace is used by Solo. 'true' if stack trace must be printed in case of errors
 	StackTrace = false
+
+	// AccountsContractName sets the name of the Accounts contract, which is a root contract present in every chain
+	AccountsContractName = "accounts"
 )
