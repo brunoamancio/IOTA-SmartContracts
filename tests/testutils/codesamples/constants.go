29f75b74cb407c482ce596e9e4e0043c34a0b7c9
Th3B0Y <brunoamancio.ti@gmail.com>
2021-03-14 19:55:46 +0100

    Refactoring

--- tests/testutils/codesamples/constants.go
+++ tests/testutils/codesamples/constants.go
@@ -1,19 +1,12 @@
 package codesamples
 
-// Used to fund address in NewSignatureSchemeWithFunds. // Defined in iotaledger/wasp/packages/testutiltestutil.RequestFundsAmount.
-const initialWalletFunds = 1337
+import "github.com/brunoamancio/IOTA-SmartContracts/tests/testutils/testconstants"
+
+const initialWalletFunds = testconstants.InitialWalletFunds
 
 // Default amount of IOTAs to transfer in unit tests.
 const transferValueIotas = 1000
 
-/* INTERESTING FACT: Calls to a smart contract require 1 EXTRA iota token to be sent to the chain it is located in.
-It is colored with the chain's color and corresponds to the request. That is how the protocol locates the backlog of
-requests to be processed. Basically, it works as a flag. After the request is processed, the token is uncolored
-and sent to the chain owner's account in the chain.
-*/
-const iotaTokensConsumedByRequest = 1
+const iotaTokensConsumedByRequest = testconstants.IotaTokensConsumedByRequest
 
-/* INTERESTING FACT: Creating a chain requires 2 iota tokens. They are colored with the chain's color,
-1 is sent to the chain's address in the value tangle, and the other is used exactly as iotaTokensConsumedByRequest.
-*/
-const iotaTokensConsumedByChain = 1
+const iotaTokensConsumedByChain = testconstants.IotaTokensConsumedByChain
--- tests/testutils/testconstants/test_constants.go
+++ tests/testutils/testconstants/test_constants.go
@@ -8,6 +8,21 @@ const (
 	// StackTrace is used by Solo. 'true' if stack trace must be printed in case of errors
 	StackTrace = false
 
+	/* INTERESTING FACT: Calls to a smart contract require 1 EXTRA iota token to be sent to the chain it is located in.
+	   It is colored with the chain's color and corresponds to the request. That is how the protocol locates the backlog of
+	   requests to be processed. Basically, it works as a flag. After the request is processed, the token is uncolored
+	   and sent to the chain owner's account in the chain.
+	*/
+	IotaTokensConsumedByRequest = 1
+
+	/* INTERESTING FACT: Creating a chain requires 2 iota tokens. They are colored with the chain's color,
+	   1 is sent to the chain's address in the value tangle, and the other is used exactly as iotaTokensConsumedByRequest.
+	*/
+	IotaTokensConsumedByChain = 1
+
+	// Used to fund address in NewSignatureSchemeWithFunds. // Defined in iotaledger/wasp/packages/testutiltestutil.RequestFundsAmount.
+	InitialWalletFunds = 1337
+
 	// AccountsContractName sets the name of the Accounts contract, which is a root contract present in every chain
 	AccountsContractName = "accounts"
 )
